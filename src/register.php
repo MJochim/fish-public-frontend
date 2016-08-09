8741f1d2b6e18d9d6983de4e902d1e89fcae1006
Markus Jochim <markusjochim@phonetik.uni-muenchen.de>
2016-08-09 21:57:27 +0200

    added mail function

--- src/register.php
+++ src/register.php
@@ -35,8 +35,51 @@ if (!is_array($registrations)) {
 	fwrite($fh, json_encode($registrations));
 	fclose($fh);
 
+	mail($newRegistration);
+
 	echo "SUCCESS";
 }
 
-?>
+function mail ($newRegistration) {
 
+	// E-Mail verschicken
+	$from = "noreply@stuts.de";
+	$subject = "Konferenz-Anmeldung";
+	$message = "";
+
+	$email = $newRegistration['email'];
+	$ccAddress = "stuts60@stuts.de";
+
+	$headers = array (
+	  "From" => $from,
+	  "Subject" => $subject,
+	  "To" => $email,
+	  "Cc" => $ccAddress
+	);
+	$crlf = "\n";
+
+	$mime = new Mail_mime(array('eol' => $crlf));
+	$mime->setTXTBody($message);
+	//$mime->setHTMLBody($message);
+
+	$mimeparams['text_encoding']="8bit";
+	$mimeparams['text_charset']="UTF-8";
+	$mimeparams['html_charset']="UTF-8";
+	$mimeparams['head_charset']="UTF-8";
+
+	$body = $mime->get($mimeparams);
+	$headers = $mime->headers($headers);
+
+
+	$mail = Mail::factory('smtp', array(
+	  "host" => "mailout.lrz.de",
+	  "localhost" => "junge-sprachwissenschaft.de"
+	));
+	
+	$return = $mail->send(array(
+	  'To'=>$email,
+	  'Cc'=>$ccAddress
+	), $headers, $body);
+}
+
+?>
