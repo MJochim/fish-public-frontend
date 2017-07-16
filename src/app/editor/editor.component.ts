d4afdba009b3f354ee7da86d0883876871355a3b
Markus Jochim <markusjochim@phonetik.uni-muenchen.de>
2017-07-16 16:45:31 +0200

    Editor now loads the conference determined by the route and does not save null conferences

--- src/app/editor/editor.component.ts
+++ src/app/editor/editor.component.ts
@@ -1,6 +1,8 @@
 import {Component, OnInit} from "@angular/core";
 import {ConferenceStoreService} from "../core/conference-store.service";
 import {Conference} from "../core/conference.interface";
+import {Subscription} from "rxjs/Subscription";
+import {ActivatedRoute, Router} from "@angular/router";
 
 @Component({
 	selector: 'confreg-editor',
@@ -11,7 +13,11 @@ export class EditorComponent implements OnInit {
 	public conference: Conference;
 	public previewMode: boolean = false;
 
-	constructor(private conferenceStoreService: ConferenceStoreService) {
+	private sub: Subscription;
+
+	constructor(private conferenceStoreService: ConferenceStoreService,
+	            private route: ActivatedRoute,
+	            private router: Router) {
 		this.conference = {
 			name: '',
 			key: '',
@@ -25,14 +31,27 @@ export class EditorComponent implements OnInit {
 	}
 
 	async ngOnInit() {
-		try {
-			this.conference = await this.conferenceStoreService.getConference('stuts60_de');
-		} catch (error) {
-			console.log(error);
-		}
+		// Get conference data
+		this.sub = this.route.params.subscribe(params => {
+			this.conferenceStoreService.getConference(params['key'])
+				.then(value => {
+					if (value) {
+						this.conference = value;
+					} else {
+						// Go back if conference data could not be loaded
+						this.router.navigate(['/']);
+					}
+				})
+				.catch(() => {
+					this.router.navigate(['/']);
+				});
+		});
 	}
 
-	edited (event) {
-		console.log(event);
+	edited(event) {
+		this.conference.registration = event;
+		if (this.conference.key) {
+			this.conferenceStoreService.pushConference(this.conference);
+		}
 	}
 }
